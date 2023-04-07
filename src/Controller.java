a1033055a9f756097060d448e97a5259fbc210ec
Manitas Bahri <manitas.bhr@gmail.com>
2023-04-07 10:53:26 +0200

    cleaning and restructuring code

--- src/Controller.java
+++ src/Controller.java
@@ -1,53 +1,45 @@
 import Model.Coord;
 import Model.Model;
 import Model.PenShape;
+import java.io.*;
+import java.util.ArrayList;
 import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.control.ColorPicker;
-import javafx.scene.control.CustomMenuItem;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Shape;
-import javafx.scene.image.WritableImage;
-import javafx.scene.Node;
-import javafx.scene.image.Image;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-import javafx.embed.swing.SwingFXUtils;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import javafx.scene.shape.*;
+import javafx.stage.*;
 import javax.imageio.ImageIO;
-import javafx.scene.image.PixelReader;
-import javafx.scene.shape.LineTo; 
-import javafx.scene.shape.MoveTo; 
-import javafx.scene.shape.Path;
-
-
 
 public class Controller {
 
   private Model model = new Model();
-  private Color appColor = Color.BLACK;
-  private int appSize = 2;
+
+  // Déclare les attricuts de l'états de l'application
   private String selectedStyle = "-fx-background-color: #D3E8EE";
 
-  // Récupère les outils de la fenêtre.
+  private Shape shape;
+
+  public Coord posStart = Coord.createCoord(0, 0);
+  public Coord posCurrent = Coord.createCoord(0, 0);
+  public Coord posOpposite = Coord.createCoord(0, 0);
+  public PenShape penShape;
+  public ArrayList<Shape> Shapes = new ArrayList<Shape>();
+  private Image previousImage;
+
+  // Déclare les attributs de la vue
   @FXML
   private Canvas canvas;
 
   @FXML
   private Label lblLog;
+
   @FXML
   private TextField fileTitle;
 
@@ -69,26 +61,37 @@ public class Controller {
   @FXML
   private ColorPicker cPicker;
 
-  private Shape shape;
-
   @FXML
   void onNew(ActionEvent event) {
+    /**
+     * Nouveau fichier
+     */
     lblLog.setText("Nouveau fichier");
   }
 
   @FXML
   void onOpen(ActionEvent event) {
+    /**
+     * Ouvrir un fichier
+     */
     lblLog.setText("Ouverture d'un fichier");
   }
 
   @FXML
   void onSave(ActionEvent event) {
+    /**
+     * Enregistrer l'image du canvas dans un fichier PNG.
+     */
+
     // Récupérer la racine de la scène
     Node node = canvas;
     Stage stage = (Stage) node.getScene().getWindow();
 
     // Capturer l'image du canvas
-    WritableImage image = new WritableImage((int) canvas.getWidth(), (int) canvas.getHeight());
+    WritableImage image = new WritableImage(
+      (int) canvas.getWidth(),
+      (int) canvas.getHeight()
+    );
     node.snapshot(null, image);
 
     // Créer un objet File pour enregistrer l'image en PNG
@@ -98,35 +101,47 @@ public class Controller {
     File file = fileChooser.showSaveDialog(stage);
 
     if (file != null) {
-        try {
-            ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
-            lblLog.setText("Fichier enregistré sous " + file.getName());
-        } catch (IOException ex) {
-            lblLog.setText("Erreur lors de l'enregistrement du fichier.");
-            ex.printStackTrace();
-        }
+      try {
+        ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
+        lblLog.setText("Fichier enregistré sous " + file.getName());
+      } 
+      
+      catch (IOException ex) {
+        lblLog.setText("Erreur lors de l'enregistrement du fichier.");
+        ex.printStackTrace();
+      }
     }
     lblLog.setText("Fichier enregistré");
   }
 
   @FXML
   void onExit(ActionEvent event) {
+    /**
+     * Fermer l'application.
+     */
+    // TODO: Afficher une boîte de dialogue pour confirmer la fermeture de l'application
     lblLog.setText("Fermeture de l'application");
     Platform.exit();
   }
 
   @FXML
   void selectColor(ActionEvent event) {
-    appColor = cPicker.getValue();
+    /**
+     * Sélectionner une couleur.
+     */
+    model.setColor(cPicker);
     lblLog.setText("Couleur modifiée");
   }
 
   @FXML
   void selectSize(ActionEvent event) {
+    /**
+     * Sélectionner la taille de l'outils.
+     */
     RadioButton selectedSize = (RadioButton) groupSize.getSelectedToggle();
     String strSize = selectedSize.getText();
-
-    appSize = Integer.parseInt(strSize.replace("px", ""));
+    
+    model.setToolSize(strSize);
     btnSize.setText(selectedSize.getText());
 
     lblLog.setText("Taille modifiée");
@@ -134,9 +149,12 @@ public class Controller {
 
   @FXML
   void selectPen(ActionEvent event) {
+    /**
+     * Sélectionner le pinceau.
+     */
     shape = null;
     cancelStyle();
-    appColor = cPicker.getValue();
+    model.setColor(cPicker);
 
     lblLog.setText("Pinceau séléctionné");
     btnPen.setStyle(selectedStyle);
@@ -144,201 +162,79 @@ public class Controller {
 
   @FXML
   void selectEraser(ActionEvent event) {
+    /**
+     * Sélectionner la gomme.
+     */
     shape = null;
     cancelStyle();
 
-    appColor = Color.WHITE;
+    model.setColor(Color.WHITE);
     lblLog.setText("Gomme séléctionnée");
     btnEraser.setStyle(selectedStyle);
   }
-  
 
   @FXML
   void selectShape(ActionEvent event) {
+    /**
+     * Sélectionner une forme.
+     */
     cancelStyle();
     btnShape.setStyle(selectedStyle);
     lblLog.setText("Forme séléctionnée");
     btnShape.setStyle(selectedStyle);
   }
-  
+
   @FXML
   void selectLine(ActionEvent event) {
+    /**
+     * Sélectionner la forme ligne.
+     */
     cancelStyle();
-    appColor = cPicker.getValue();
+    model.setColor(cPicker);
     lblLog.setText("Ligne sélectionnée");
     btnLine.setStyle(selectedStyle);
-    shape = new javafx.scene.shape.Line();
+    shape = new Line();
     btnShape.setStyle(selectedStyle);
   }
-  
+
   @FXML
   void selectRectangle(ActionEvent event) {
+    /**
+     * Sélectionner la forme rectangle.
+     */
     cancelStyle();
-    appColor = cPicker.getValue();
+    model.setColor(cPicker);
+    
     lblLog.setText("Rectangle sélectionné");
     btnRect.setStyle(selectedStyle);
     btnShape.setStyle(selectedStyle);
-    shape = new javafx.scene.shape.Rectangle();
+    shape = new Rectangle();
   }
-  
+
   @FXML
   void selectCircle(ActionEvent event) {
+    /**
+     * Sélectionner la forme cercle.
+     */
     cancelStyle();
     btnShape.setStyle(selectedStyle);
-    appColor = cPicker.getValue();
+    model.setColor(cPicker);
     lblLog.setText("Cercle sélectionné");
     btnCircle.setStyle(selectedStyle);
-    shape = new javafx.scene.shape.Circle();
+    shape = new Circle();
   }
-  
+
   @FXML
   void selectTriangle(ActionEvent event) {
+    /**
+     * Sélectionner la forme triangle.
+     */
     cancelStyle();
-    appColor = cPicker.getValue();
+    model.setColor(cPicker);
     lblLog.setText("Cercle sélectionné");
     btnTriangle.setStyle(selectedStyle);
     btnShape.setStyle(selectedStyle);
-    shape = new javafx.scene.shape.Polygon();
-  }
-  
-  
-  // Méthode pour dessiner une ligne
-  private void drawLine(GraphicsContext gc, Coord start, Coord end) {
-    gc.setStroke(appColor);
-    gc.setLineWidth(appSize);
-    gc.strokeLine(start.x, start.y, end.x, end.y);
-  }
-
-  // Méthode pour dessiner un rectangle
-  private void drawRectangle(GraphicsContext gc, Coord start, Coord end) {
-    gc.setStroke(appColor);
-    gc.setLineWidth(appSize);
-
-    double width = Math.abs(start.x - end.x);
-    double height = Math.abs(start.y - end.y);
-    double x = Math.min(start.x, end.x);
-    double y = Math.min(start.y, end.y);
-
-    gc.strokeRect(x, y, width, height);
-  }
-
-  //Méthode pour dessiner un cercle
-  private void drawCircle(GraphicsContext gc, Coord start, Coord end) {
-    gc.setStroke(appColor);
-    gc.setLineWidth(appSize);
-
-    double radius = start.getDistance(end);
-    double centerX = start.x;
-    double centerY = start.y;
-
-    gc.strokeOval(centerX - radius, centerY - radius, radius * 2, radius * 2);
-  }
-
-  //Méthode pour dessiner un triangle
-  private void drawTriangle(GraphicsContext gc, Coord point1, Coord point2, Coord point3) {
-    gc.setStroke(appColor);
-    gc.setLineWidth(appSize);
-
-    double[] xPoints = new double[]{point1.x, point2.x, point3.x};
-    double[] yPoints = new double[]{point1.y, point2.y, point3.y};
-
-    gc.strokePolygon(xPoints, yPoints, 3);
-  }
-
-
-
-  public Coord startpos = Coord.createCoord(0,0);
-  public Coord currentpos = Coord.createCoord(0,0);
-  public Coord oppositepos = Coord.createCoord(0,0);
-  public PenShape penShape;
-  public ArrayList<PenShape> Shapes = new ArrayList<PenShape>();
-  private Image previousImage;
-
-
-  @FXML
-  public void initialize() {
-    GraphicsContext g = canvas.getGraphicsContext2D();
-
-    canvas.setOnMousePressed(e -> {
-        startpos = Coord.getCoordMouse(e, appSize);
-        currentpos = startpos;
-        penShape = new PenShape(startpos, appColor, appSize);
-
-        g.setStroke(appColor);
-        g.setLineWidth(appSize);
-        g.beginPath();
-        g.moveTo(e.getX(), e.getY());
-        previousImage = canvas.snapshot(null, null); // enregistrer l'image actuelle
-    });
-
-    canvas.setOnMouseDragged(e -> {
-      Coord mouse = Coord.getCoordMouse(e, appSize);
-      currentpos = mouse;
-  
-      if (shape != null && shape instanceof javafx.scene.shape.Line) {
-          g.clearRect(0, 0, canvas.getWidth(), canvas.getHeight()); // Effacer le contenu du canvas
-          g.drawImage(previousImage, 0, 0); // Restaurer l'image précédente
-          drawLine(g, startpos, currentpos);
-          ((javafx.scene.shape.Line) shape).setEndX(currentpos.x);
-          ((javafx.scene.shape.Line) shape).setEndY(currentpos.y);
-      } else if (shape != null && shape instanceof javafx.scene.shape.Rectangle) {
-          g.clearRect(0, 0, canvas.getWidth(), canvas.getHeight()); // Effacer le contenu du canvas
-          g.drawImage(previousImage, 0, 0); // Restaurer l'image précédente
-          drawRectangle(g, startpos, currentpos);
-          //((javafx.scene.shape.Line) shape).setEndX(currentpos.x);
-          //((javafx.scene.shape.Line) shape).setEndY(currentpos.y);
-      } else if (shape != null && shape instanceof javafx.scene.shape.Circle) {
-          g.clearRect(0, 0, canvas.getWidth(), canvas.getHeight()); // Effacer le contenu du canvas
-          g.drawImage(previousImage, 0, 0); // Restaurer l'image précédente
-          drawCircle(g, startpos, currentpos);
-      } else if (shape != null && shape instanceof javafx.scene.shape.Polygon) {
-          oppositepos.x = currentpos.x - currentpos.getDistance(startpos);
-          oppositepos.y = currentpos.y;
-          g.clearRect(0, 0, canvas.getWidth(), canvas.getHeight()); // Effacer le contenu du canvas
-          g.drawImage(previousImage, 0, 0); // Restaurer l'image précédente
-          drawTriangle(g, startpos, currentpos, oppositepos);
-      } else {
-          double cpx = (startpos.x + currentpos.x) / 2.0;
-          double cpy = (startpos.y + currentpos.y) / 2.0;
-    
-          g.quadraticCurveTo(startpos.x, startpos.y, cpx, cpy);
-          g.stroke();
-          g.beginPath();
-          g.moveTo(cpx, cpy);
-
-          startpos = currentpos;
-          penShape.addCoord(currentpos);
-          previousImage = canvas.snapshot(null, null); // enregistrer la nouvelle image
-      }
-    });
-
-    canvas.setOnMouseReleased(e -> {
-        Coord mouse = Coord.getCoordMouse(e, appSize);
-        currentpos = mouse;
-
-        if (shape != null && shape instanceof javafx.scene.shape.Line) {
-            g.clearRect(0, 0, canvas.getWidth(), canvas.getHeight()); // Effacer le contenu du canvas
-            g.drawImage(previousImage, 0, 0); // Restaurer l'image précédente
-            drawLine(g, startpos, currentpos);
-            ((javafx.scene.shape.Line) shape).setEndX(currentpos.x);
-            ((javafx.scene.shape.Line) shape).setEndY(currentpos.y);
-        } else if (shape != null && shape instanceof javafx.scene.shape.Rectangle){
-            g.clearRect(0, 0, canvas.getWidth(), canvas.getHeight()); // Effacer le contenu du canvas
-            g.drawImage(previousImage, 0, 0); // Restaurer l'image précédente
-            drawRectangle(g, startpos, currentpos);
-        } else if (shape != null && shape instanceof javafx.scene.shape.Circle){
-            g.clearRect(0, 0, canvas.getWidth(), canvas.getHeight()); // Effacer le contenu du canvas
-            g.drawImage(previousImage, 0, 0); // Restaurer l'image précédente
-            drawCircle(g, startpos, currentpos);
-        } else if (shape != null && shape instanceof javafx.scene.shape.Circle){
-            g.clearRect(0, 0, canvas.getWidth(), canvas.getHeight()); // Effacer le contenu du canvas
-            g.drawImage(previousImage, 0, 0); // Restaurer l'image précédente
-            drawTriangle(g, startpos, currentpos, oppositepos);
-        } else {
-            penShape.addCoord(currentpos);
-            Shapes.add(penShape);
-        }
-    });
+    shape = new Polygon();
   }
 
   @FXML
@@ -347,39 +243,141 @@ public class Controller {
     btnEraser.setStyle(defaultStyle);
     btnPen.setStyle(defaultStyle);
     btnShape.setStyle(defaultStyle);
-    btnShape.setStyle(defaultStyle);;
+    btnShape.setStyle(defaultStyle);
   }
 
-  public void eraseColor(GraphicsContext gc, Coord c) {
-    PixelReader pixelReader = gc.getCanvas().snapshot(null, null).getPixelReader();
-    Color color = pixelReader.getColor((int)c.x, (int)c.y);
-    eraseColorRecursive(gc, pixelReader, color, c);
-  }
 
-  private void eraseColorRecursive(GraphicsContext gc, PixelReader pixelReader, Color color, Coord c) {
-    if (c.x < 0 || c.y < 0 || c.x >= gc.getCanvas().getWidth() || c.y >= gc.getCanvas().getHeight()) {
-        return;
-    }
-    if (!pixelReader.getColor((int)c.x, (int)c.y).equals(color)) {
-        return;
-    }
-    gc.setStroke(Color.WHITE);
-    gc.setLineWidth(appSize);
-    gc.moveTo(c.x, c.y);
-    gc.stroke();
-    ArrayList<Coord> neighbors = getNeighbors(c);
-    for (Coord neighbor : neighbors) {
-        eraseColorRecursive(gc, pixelReader, color, neighbor);
-    }
-  }
-  private ArrayList<Coord> getNeighbors(Coord c) {
-    ArrayList<Coord> neighbors = new ArrayList<>();
-    neighbors.add(Coord.createCoord(c.x - 1, c.y));
-    neighbors.add(Coord.createCoord(c.x + 1, c.y));
-    neighbors.add(Coord.createCoord(c.x, c.y - 1));
-    neighbors.add(Coord.createCoord(c.x, c.y + 1));
-    System.out.println(neighbors);
-    return neighbors;
-  }
+  @FXML
+  public void initialize() {
+    GraphicsContext gc = canvas.getGraphicsContext2D();
 
+    canvas.setOnMousePressed(e -> {
+      // Récupère les coordonnées de la souris
+      posStart = Coord.getCoordMouse(e, model.getToolSize());
+      posCurrent = posStart;
+
+      // Créer une nouvelle forme pinceau
+      penShape = new PenShape(posStart, model.getColor(), model.getToolSize());
+
+      gc.setStroke(model.getColor());
+      gc.setLineWidth(model.getToolSize());
+      gc.beginPath();
+      gc.moveTo(e.getX(), e.getY());
+
+      // Sauvegarder l'image précédente
+      previousImage = canvas.snapshot(null, null);
+    });
+
+    canvas.setOnMouseDragged(e -> {
+      // Récupère les coordonnées de la souris
+      Coord mouse = Coord.getCoordMouse(e, model.getToolSize());
+      posCurrent = mouse;
+
+      if (shape instanceof Line) {
+        //  Restaurer l'image précédente
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight()); 
+        gc.drawImage(previousImage, 0, 0); // Restaurer l'image précédente
+       
+        model.drawLine(gc, posStart, posCurrent);
+       
+        ((Line) shape).setEndX(posCurrent.x);
+        ((Line) shape).setEndY(posCurrent.y);
+      } 
+      
+      else if (shape instanceof Rectangle) {
+        // Restaurer l'image précédente
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight()); 
+        gc.drawImage(previousImage, 0, 0);
+        
+        // Dessiner le rectangle
+        model.drawRectangle(gc, posStart, posCurrent);
+      } 
+      
+      else if (shape instanceof Circle) {
+        // Restaurer l'image précédente
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight()); 
+        gc.drawImage(previousImage, 0, 0); 
+        
+        // Dessiner le cercle
+        model.drawCircle(gc, posStart, posCurrent);
+      } 
+      
+      else if (shape instanceof Polygon) {
+        // Restaurer l'image précédente
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight()); 
+        gc.drawImage(previousImage, 0, 0); 
+        
+        posOpposite.x = posCurrent.x - posCurrent.getDistance(posStart);
+        posOpposite.y = posCurrent.y;
+        
+        model.drawTriangle(gc, posStart, posCurrent, posOpposite);
+      } 
+      
+      else {
+        double centerX = (posStart.x + posCurrent.x) / 2.0;
+        double centerY = (posStart.y + posCurrent.y) / 2.0;
+
+        gc.quadraticCurveTo(posStart.x, posStart.y, centerX, centerY);
+        gc.stroke();
+        gc.beginPath();
+        gc.moveTo(centerX, centerY);
+
+        posStart = posCurrent;
+        penShape.addCoord(posCurrent);
+        previousImage = canvas.snapshot(null, null); // enregistrer la nouvelle image
+      }
+    });
+
+    canvas.setOnMouseReleased(e -> {
+      // Récupère les coordonnées de la souris
+      Coord mouse = Coord.getCoordMouse(e, model.getToolSize());
+      posCurrent = mouse;
+
+      if (shape instanceof Line) {
+        // Restaurer l'image précédente
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight()); 
+        gc.drawImage(previousImage, 0, 0);
+
+        // Dessiner la ligne
+        model.drawLine(gc, posStart, posCurrent);
+
+        // Definie les coordonnées de la ligne
+        ((Line) shape).setEndX(posCurrent.x);
+        ((Line) shape).setEndY(posCurrent.y);
+      } 
+      
+      else if (shape instanceof Rectangle) {
+        // Restaurer l'image précédente
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight()); 
+        gc.drawImage(previousImage, 0, 0);
+        
+        // Dessiner le rectangle
+        model.drawRectangle(gc, posStart, posCurrent);
+      } 
+      
+      else if (shape instanceof Circle) {
+        // Restaurer l'image précédente
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight()); 
+        gc.drawImage(previousImage, 0, 0);
+        
+        // Dessiner le cercle
+        model.drawCircle(gc, posStart, posCurrent);
+      } 
+      
+      else if (shape instanceof Circle) {
+        // Restaurer l'image précédente
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight()); 
+        gc.drawImage(previousImage, 0, 0);
+        
+        // Dessiner le triangle
+        model.drawTriangle(gc, posStart, posCurrent, posOpposite);
+      } 
+      
+      else {
+        penShape.addCoord(posCurrent);
+        Shapes.add(penShape);
+      }
+    });
+
+  }
 }
--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,8 +1,114 @@
 package Model;
 
+import java.util.ArrayList;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.paint.Color;
+
 public class Model {
 
-  public int getSize(String strSize) {
-    return Integer.parseInt(strSize.replace("px", ""));
+  private Color toolColor = Color.BLACK;
+  private int toolSize = 1;
+
+  public void setColor(Color color) {
+    toolColor = color;
+  }
+
+  public void setColor(ColorPicker cPicker) {
+    toolColor = cPicker.getValue();
+  }
+
+  public void setToolSize(int size) {
+    toolSize = size;
+  }
+
+  public void setToolSize(String size) {
+    toolSize = Integer.parseInt(size.replace("px", ""));
+  }
+
+  public Color getColor() {
+    return toolColor;
+  }
+
+  public int getToolSize() {
+    return toolSize;
+  }
+
+  public ArrayList<Coord> getNeighbors(Coord c) {
+    ArrayList<Coord> neighbors = new ArrayList<>();
+    neighbors.add(Coord.createCoord(c.x - 1, c.y));
+    neighbors.add(Coord.createCoord(c.x + 1, c.y));
+    neighbors.add(Coord.createCoord(c.x, c.y - 1));
+    neighbors.add(Coord.createCoord(c.x, c.y + 1));
+    System.out.println(neighbors);
+    return neighbors;
+  }
+
+  public void drawLine(GraphicsContext gc, Coord start, Coord end) {
+    /**
+     * Dessiner une ligne.
+     *
+     * @param gc   Le contexte graphique
+     * @param start Les coordonnées de départ
+     * @param end Les coordonnées d'arrivée
+     */
+    gc.setStroke(toolColor);
+    gc.setLineWidth(toolSize);
+    gc.strokeLine(start.x, start.y, end.x, end.y);
+  }
+
+  public void drawRectangle(GraphicsContext gc, Coord start, Coord end) {
+    /**
+     * Dessiner un rectangle.
+     *
+     * @param gc   Le contexte graphique
+     * @param start Les coordonnées de départ
+     * @param end Les coordonnées d'arrivée
+     */
+    gc.setStroke(toolColor);
+    gc.setLineWidth(toolSize);
+
+    double width = Math.abs(start.x - end.x);
+    double height = Math.abs(start.y - end.y);
+    double x = Math.min(start.x, end.x);
+    double y = Math.min(start.y, end.y);
+
+    gc.strokeRect(x, y, width, height);
+  }
+
+  public void drawCircle(GraphicsContext gc, Coord start, Coord end) {
+    /**
+     * Dessiner un cercle.
+     *
+     * @param gc   Le contexte graphique
+     * @param start Les coordonnées de départ
+     * @param end Les coordonnées d'arrivée
+     */
+    gc.setStroke(toolColor);
+    gc.setLineWidth(toolSize);
+
+    double radius = start.getDistance(end);
+    double centerX = start.x;
+    double centerY = start.y;
+
+    gc.strokeOval(centerX - radius, centerY - radius, radius * 2, radius * 2);
+  }
+
+  public void drawTriangle(GraphicsContext gc, Coord x1, Coord x2, Coord x3) {
+    /**
+     * Dessiner un triangle.
+     *
+     * @param gc Le contexte graphique
+     * @param x1 Les coordonnées du premier point
+     * @param x2 Les coordonnées du deuxième point
+     * @param x3 Les coordonnées du troisième point
+     */
+    gc.setStroke(toolColor);
+    gc.setLineWidth(toolSize);
+
+    double[] xPoints = new double[] { x1.x, x2.x, x3.x };
+    double[] yPoints = new double[] { x1.y, x2.y, x3.y };
+
+    gc.strokePolygon(xPoints, yPoints, 3);
   }
 }
